a60b319551a096d32f08a5281c397f474095b837
UmbertoSerino <serumberto@gmail.com>
2024-02-07 18:09:06 +0100

    change 'string' img to thumb

--- resources/views/comics/guest/edit.blade.php
+++ resources/views/comics/guest/edit.blade.php
@@ -4,16 +4,19 @@
 <section class="container-heros-cards">
 
   <h1 class="m-5">Modifica Card:</h1>
-  <form class="row g-3" method="POST" action="{{ route('comics.guest.update', $comic->id) }}" >
+  {{-- nell'edit modificare il metodo da post ad put utilizzando @method --}}
+  <form class="row g-3" method="POST" action="{{ route('comics.guest.update', $comic->id) }}">
+    {{-- csrf fondamentale per poter inviare il form correttamente --}}
      @csrf
      @method('PUT')
     <div class="col-md-6">
       <label for="title" class="form-label">Title: </label>
-      <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}">
+      {{-- nel value inseriamo il valore originario del file --}}
+      <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}" minlength="18" maxlength="30">
     </div>
     <div class="col-md-6">
-      <label for="thumb" class="form-label">Url Img:</label>
-      <input type="text" class="form-control" id="thumb" name="thumb" value="{{ $comic->img }}">
+      <label for="img" class="form-label">Url Img:</label>
+      <input type="text" class="form-control" id="img" name="img" value="{{ $comic->img }}">
     </div>
     <div class="col-12">
       <label for="price" class="form-label">Price:</label>
@@ -32,11 +35,12 @@
       <input type="text" class="form-control" id="type" name="type" placeholder="Comic Book" value="{{ $comic->type }}">
     </div>
     <div class="col-12">
+      {{-- nella textarea il value va tra i tag --}}
       <textarea name="description" id="description" cols="100" rows="6">{{ $comic->description }}"</textarea>
     </div>
 
     <div class="col-12">
-      <button type="submit" class="btn btn-primary">Sign in</button>
+      <button type="submit" class="btn btn-primary">Salva</button>
     </div>
   </form>
 
